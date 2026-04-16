9f14515a0e0c6cfde5ea62581014b9758f540016
Arjun G Kanagal <arjungk28@gmail.com>
2026-04-16 11:06:27 +0530

    Phase2: load index for tree construction

--- tree.c
+++ tree.c
@@ -132,6 +132,9 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 int tree_from_index(ObjectID *id_out) {
     // TODO: Implement recursive tree building
     // (See Lab Appendix for logical steps)
-    (void)id_out;
+    Index idx;
+    index_load(&idx);
+
+    (void)tree_id;
     return -1;
 }
